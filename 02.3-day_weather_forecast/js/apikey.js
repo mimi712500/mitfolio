4d68c6eb173da2df682e2985dfb2b506005abe71
mimi <mimi712500@gmail.com>
2026-09-20 23:14:19 +0900

    수정

--- 02.3-day_weather_forecast/js/apikey.js
+++ 02.3-day_weather_forecast/js/apikey.js
@@ -1,6 +1,6 @@
 const config = {
     WEWTHER_API_KEY:"14f81daf1e494a66afe71318241910",
-    MAPS_PLATFORM_API_KEY:"AIzaSyB14aweftuWa4DkztRzk5_JSU-e9f2xQWo",
+    MAPS_PLATFORM_API_KEY:"AIzaSyCUMIH0bHdE9D4tGQlTfRm2vF0jUkWNWhE",
 };
 
 export default config;
